67f39b79991f6e267cb9ca79e589f58dfaae5ff8
Nikita <nikita_tkachev_2001@mail.ru>
2023-03-30 01:58:41 +0300

    sorted list dictionary tests

--- sorted_list_dict_test.py
+++ sorted_list_dict_test.py
@@ -2,7 +2,7 @@ from sorted_list_dict import SortedListDict
 
 
 def create_sorted_dict():
-    return SortedListDict.from_array([('asd', 10), ('bfe', 100), ('cmd', 70)])
+    return SortedListDict.from_array([('aaa', 10), ('bbb', 20), ('ccc', 30)])
 
 
 def test_smoke_set():
@@ -10,53 +10,158 @@ def test_smoke_set():
 
     assert str(d) == "[]"
 
-    d.set('asd', 13)
-    assert str(d) == "[('asd', 13)]"
+    d.set('aaa', 10)
+    assert str(d) == "[('aaa', 10)]"
 
-    d.set('ase', 43)
-    assert str(d) == "[('asd', 13), ('ase', 43)]"
+    d.set('bbb', 20)
+    assert str(d) == "[('aaa', 10), ('bbb', 20)]"
 
-    d.set('trf', 34)
-    assert str(d) == "[('asd', 13), ('ase', 43), ('trf', 34)]"
+    d.set('ccc', 30)
+    assert str(d) == "[('aaa', 10), ('bbb', 20), ('ccc', 30)]"
+
+
+def test_set_into_empty():
+    d = SortedListDict()
+
+    assert d.set('aaa', 10) is None
+
+    assert str(d) == "[('aaa', 10)]"
+
+
+def test_set_key_with_lower_value():
+    d = SortedListDict()
+
+    assert d.set('ccc', 30) is None
+    assert str(d) == "[('ccc', 30)]"
+
+    assert d.set('bbb', 20) is None
+    assert str(d) == "[('bbb', 20), ('ccc', 30)]"
+
+    assert d.set('aaa', 10) is None
+    assert str(d) == "[('aaa', 10), ('bbb', 20), ('ccc', 30)]"
+
+
+def test_set_key_with_greater_value():
+    d = SortedListDict()
+
+    assert d.set('aaa', 10) is None
+    assert str(d) == "[('aaa', 10)]"
+
+    assert d.set('bbb', 20) is None
+    assert str(d) == "[('aaa', 10), ('bbb', 20)]"
+
+    assert d.set('ccc', 30) is None
+    assert str(d) == "[('aaa', 10), ('bbb', 20), ('ccc', 30)]"
 
 
 def test_set_key_already_exist_one_element():
     d = SortedListDict()
 
-    assert d.set('asd', 10) is None
+    assert d.set('aaa', 10) is None
 
-    assert d.set('asd', 20) == 10
+    assert d.set('aaa', 20) == 10
 
-    assert str(d) == "[('asd', 20)]"
+    assert str(d) == "[('aaa', 20)]"
 
 
 def test_set_key_already_exist_many_elements():
     d = SortedListDict()
 
-    assert d.set('asd', 10) is None
+    assert d.set('aaa', 10) is None
 
-    assert d.set('bfe', 20) is None
+    assert d.set('bbb', 20) is None
 
-    assert d.set('cmd', 70) is None
+    assert d.set('ccc', 30) is None
 
-    assert d.set('bfe', 100) == 20
+    assert d.set('bbb', 100) == 20
 
-    assert str(d) == "[('asd', 10), ('bfe', 100), ('cmd', 70)]"
+    assert str(d) == "[('aaa', 10), ('bbb', 100), ('ccc', 30)]"
 
 
 def test_smoke_get():
     d = create_sorted_dict()
 
-    assert d.get('asd') == 10
+    assert d.get('aaa') == 10
+
+
+def test_get_from_empty():
+    d = SortedListDict()
+
+    assert d.get('aaa') is None
 
 
 def test_get_non_existent_key():
     d = create_sorted_dict()
 
-    assert d.get('gfv') is None
+    assert d.get('ddd') is None
 
 
-def test_get_from_empty_dict():
+def test_remove_from_empty():
     d = SortedListDict()
 
-    assert d.get('gft') is None
+    assert d.remove('aaa') is None
+
+
+def test_remove_with_single_element():
+    d = SortedListDict()
+
+    assert d.set('aaa', 10) is None
+    assert str(d) == "[('aaa', 10)]"
+
+    assert d.remove('aaa') == 10
+    assert str(d) == "[]"
+
+
+def test_remove_head_among_several_element():
+    d = create_sorted_dict()
+
+    assert d.remove('aaa') == 10
+    assert str(d) == "[('bbb', 20), ('ccc', 30)]"
+
+
+def test_remove_middle_among_several_elements():
+    d = create_sorted_dict()
+
+    assert d.remove('bbb') == 20
+    assert str(d) == "[('aaa', 10), ('ccc', 30)]"
+
+
+def test_remove_tail_among_several_elements():
+    d = create_sorted_dict()
+
+    assert d.remove('ccc') == 30
+    assert str(d) == "[('aaa', 10), ('bbb', 20)]"
+
+
+def test_remove_non_existing_head_element_among_one_element():
+    d = SortedListDict()
+
+    d.set('bbb', 20)
+
+    assert d.remove('aaa') is None
+
+
+def test_remove_non_existing_tail_element_among_one_element():
+    d = SortedListDict()
+
+    d.set('bbb', 20)
+
+    assert d.remove('ccc') is None
+
+
+def test_remove_non_existing_head_element_among_several_elements():
+    d = create_sorted_dict()
+
+    assert d.remove('aa') is None
+
+
+def test_remove_non_existing_middle_element_among_several_elements():
+    d = create_sorted_dict()
+
+    assert d.remove('bbc') is None
+
+
+def test_remove_non_existing_tail_element_among_several_elements():
+    d = create_sorted_dict()
+
+    assert d.remove('ddd') is None
